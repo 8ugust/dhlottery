d9388e4b6b9efe6c59ef6beda91d09008997490e
= <=>
2024-08-03 16:36:48 +0900

    Email

--- dhlottery.py
+++ dhlottery.py
@@ -6,14 +6,11 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from configparser import ConfigParser
+from email.mime.text import MIMEText
 from selenium import webdriver
-from PIL import Image
-import pytesseract
 import datetime
-import requests
+import smtplib
 import time
-import re
-import io
 
 
 # =================== =================== =================== ===================
@@ -27,6 +24,7 @@ chrome_options.add_argument("headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--log-level=3')
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument('--blink-settings=imagesEnabled=false')
 
 caps = DesiredCapabilities().CHROME
 caps["pageLoadStrategy"] = "none"
@@ -51,7 +49,6 @@ config = ConfigParser()
 config.read('./conf.ini')
 id = config['lottery']['id']
 pw = config['lottery']['pw']
-fa = config['lottery']['fa']
 
 form_id.send_keys(id)
 form_pw.send_keys(pw)
@@ -81,45 +78,65 @@ time.sleep(1)
 # =================== =================== ===================
 # Check Remain Money
 # =================== =================== ===================
-print(driver.current_url)
 cMoney = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'money')))
 money = cMoney.find_element(By.TAG_NAME, 'strong').get_attribute('innerHTML')
 money = money.replace(',', '').replace('원', '')
 
-if money == "32000":
-    Select(wait.until(EC.visibility_of_element_located((By.ID, 'EcAmt')))).select_by_value('5000')
-    btnWrap = wait.until(EC.visibility_of_element_located((By.ID, 'btn2')))
-    btn = btnWrap.find_element(By.TAG_NAME, 'button')
-    btn.click()
-    time.sleep(1)
+if money <= "50000":
 
-    # Switch Current Window to Popup
-    parent = driver.current_window_handle
-    for window in driver.window_handles:
-        if window != parent:
-            driver.switch_to.window(window)
+    sender = 'gks83123@gmail.com'
+    receiver = 'gks831@kakao.com'
 
-    # Get Password Keypad Image
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\gks83\AppData\Local\tesseract.exe'
-    kpdImg = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'kpd-image-button')))
-    keypad = wait.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'kpd-data')))
-    response = requests.get(kpdImg.get_attribute('src'))
-    img = Image.open(io.BytesIO(response.content))
+    msg = MIMEText("잔액이 부족합니다.")
+    msg['Subject'] = "[DHL] 잔액부족 알림"
+    msg['From'] = sender
+    msg['To'] = receiver
 
-    # Keypad to Array
-    txt = pytesseract.image_to_string(img, config='--psm 6')
-    txtArr = re.findall(r'\d', txt)
-    numbers = sorted(set(txtArr), key=txtArr.index)
+    config = ConfigParser()
+    config.read('conf.ini')
+    email = config['gmail']['email']
+    pswrd = config['gmail']['pswrd']
 
-    keypad = list(filter(lambda pad: pad.get_attribute('data-action').split(":")[0] == 'data', keypad))
-    for factor in fa:
-        for idx, num in enumerate(numbers):
-            if factor == num:
-                keypad[idx].click()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, pswrd)
+
+    server.sendmail(sender, receiver, msg.as_string())
+    server.quit()
+
+    # Select(wait.until(EC.visibility_of_element_located((By.ID, 'EcAmt')))).select_by_value('5000')
+    # btnWrap = wait.until(EC.visibility_of_element_located((By.ID, 'btn2')))
+    # btn = btnWrap.find_element(By.TAG_NAME, 'button')
+    # btn.click()
+    # time.sleep(1)
+
+    # # Switch Current Window to Popup
+    # parent = driver.current_window_handle
+    # for window in driver.window_handles:
+    #     if window != parent:
+    #         driver.switch_to.window(window)
+
+    # # Get Password Keypad Image
+    # pytesseract.pytesseract.tesseract_cmd = r'C:\Users\gks83\AppData\Local\tesseract.exe'
+    # kpdImg = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'kpd-image-button')))
+    # keypad = wait.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'kpd-data')))
+    # response = requests.get(kpdImg.get_attribute('src'))
+    # img = Image.open(io.BytesIO(response.content))
+
+    # # Keypad to Array
+    # txt = pytesseract.image_to_string(img, config='--psm 6')
+    # txtArr = re.findall(r'\d', txt)
+    # numbers = sorted(set(txtArr), key=txtArr.index)
+
+    # keypad = list(filter(lambda pad: pad.get_attribute('data-action').split(":")[0] == 'data', keypad))
+    # for factor in fa:
+    #     for idx, num in enumerate(numbers):
+    #         if factor == num:
+    #             keypad[idx].click()
     
-    time.sleep(1)
-    Alert(driver).accept()
-    driver.switch_to.window(parent)
+    # time.sleep(1)
+    # Alert(driver).accept()
+    # driver.switch_to.window(parent)
 
 
 
