bc207a3449c4a91c0969b038f8342f500dfa4b33
shmin <gks831@naver.com>
2024-08-05 22:54:09 +0900

    Del Try

--- dhlottery.py
+++ dhlottery.py
@@ -35,159 +35,156 @@ driver.get(url='https://dhlottery.co.kr/user.do?method=login&returnUrl=/payment.
 
 
 
-try:
-    # =================== =================== ===================
-    # Login
-    # =================== =================== ===================
-    wait = WebDriverWait(driver, 5)
-    login_form = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'form')))
-    form_pw = wait.until(EC.visibility_of_element_located((By.NAME, 'password')))
-    form_id = wait.until(EC.visibility_of_element_located((By.NAME, 'userId')))
-    button = login_form.find_element(By.TAG_NAME, 'a')
+
+# =================== =================== ===================
+# Login
+# =================== =================== ===================
+wait = WebDriverWait(driver, 5)
+login_form = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'form')))
+form_pw = wait.until(EC.visibility_of_element_located((By.NAME, 'password')))
+form_id = wait.until(EC.visibility_of_element_located((By.NAME, 'userId')))
+button = login_form.find_element(By.TAG_NAME, 'a')
+
+config = ConfigParser()
+config.read('./conf.ini')
+id = config['lottery']['id']
+pw = config['lottery']['pw']
+
+form_id.send_keys(id)
+form_pw.send_keys(pw)
+button.click()
+time.sleep(1)
+
+
+
+
+
+# =================== =================== ===================
+# Close Popup
+# Deprecated : Not Used in Payment Method View.
+# =================== =================== ===================
+# parent = driver.current_window_handle
+# uselessWindows = driver.window_handles
+# for window in uselessWindows:
+#     if window != parent:
+#         driver.switch_to.window(window)
+#         driver.close()
+# driver.switch_to.window(parent)
+
+
+
+
+
+# =================== =================== ===================
+# Check Remain Money
+# =================== =================== ===================
+cMoney = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'money')))
+money = cMoney.find_element(By.TAG_NAME, 'strong').get_attribute('innerHTML')
+money = money.replace(',', '').replace('원', '')
+print("[Log] Login Success")
+
+if money == "0":
+
+    sender = 'gks83123@gmail.com'
+    receiver = 'gks831@kakao.com'
+
+    msg = MIMEText("잔액이 부족합니다.")
+    msg['Subject'] = "[DHL] 잔액부족 알림"
+    msg['From'] = sender
+    msg['To'] = receiver
 
     config = ConfigParser()
-    config.read('./conf.ini')
-    id = config['lottery']['id']
-    pw = config['lottery']['pw']
+    config.read('conf.ini')
+    email = config['gmail']['email']
+    pswrd = config['gmail']['pswrd']
 
-    form_id.send_keys(id)
-    form_pw.send_keys(pw)
-    button.click()
-    time.sleep(1)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, pswrd)
 
+    server.sendmail(sender, receiver, msg.as_string())
+    server.quit()
 
+    # Select(wait.until(EC.visibility_of_element_located((By.ID, 'EcAmt')))).select_by_value('5000')
+    # btnWrap = wait.until(EC.visibility_of_element_located((By.ID, 'btn2')))
+    # btn = btnWrap.find_element(By.TAG_NAME, 'button')
+    # btn.click()
+    # time.sleep(1)
 
-
-
-    # =================== =================== ===================
-    # Close Popup
-    # Deprecated : Not Used in Payment Method View.
-    # =================== =================== ===================
+    # # Switch Current Window to Popup
     # parent = driver.current_window_handle
-    # uselessWindows = driver.window_handles
-    # for window in uselessWindows:
+    # for window in driver.window_handles:
     #     if window != parent:
     #         driver.switch_to.window(window)
-    #         driver.close()
+
+    # # Get Password Keypad Image
+    # pytesseract.pytesseract.tesseract_cmd = r'C:\Users\gks83\AppData\Local\tesseract.exe'
+    # kpdImg = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'kpd-image-button')))
+    # keypad = wait.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'kpd-data')))
+    # response = requests.get(kpdImg.get_attribute('src'))
+    # img = Image.open(io.BytesIO(response.content))
+
+    # # Keypad to Array
+    # txt = pytesseract.image_to_string(img, config='--psm 6')
+    # txtArr = re.findall(r'\d', txt)
+    # numbers = sorted(set(txtArr), key=txtArr.index)
+
+    # keypad = list(filter(lambda pad: pad.get_attribute('data-action').split(":")[0] == 'data', keypad))
+    # for factor in fa:
+    #     for idx, num in enumerate(numbers):
+    #         if factor == num:
+    #             keypad[idx].click()
+    
+    # time.sleep(1)
+    # Alert(driver).accept()
     # driver.switch_to.window(parent)
 
 
 
 
 
-    # =================== =================== ===================
-    # Check Remain Money
-    # =================== =================== ===================
-    cMoney = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'money')))
-    money = cMoney.find_element(By.TAG_NAME, 'strong').get_attribute('innerHTML')
-    money = money.replace(',', '').replace('원', '')
-    print("[Log] Login Success")
-
-    if money == "0":
-
-        sender = 'gks83123@gmail.com'
-        receiver = 'gks831@kakao.com'
-
-        msg = MIMEText("잔액이 부족합니다.")
-        msg['Subject'] = "[DHL] 잔액부족 알림"
-        msg['From'] = sender
-        msg['To'] = receiver
-
-        config = ConfigParser()
-        config.read('conf.ini')
-        email = config['gmail']['email']
-        pswrd = config['gmail']['pswrd']
-
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(email, pswrd)
-
-        server.sendmail(sender, receiver, msg.as_string())
-        server.quit()
-
-        # Select(wait.until(EC.visibility_of_element_located((By.ID, 'EcAmt')))).select_by_value('5000')
-        # btnWrap = wait.until(EC.visibility_of_element_located((By.ID, 'btn2')))
-        # btn = btnWrap.find_element(By.TAG_NAME, 'button')
-        # btn.click()
-        # time.sleep(1)
-
-        # # Switch Current Window to Popup
-        # parent = driver.current_window_handle
-        # for window in driver.window_handles:
-        #     if window != parent:
-        #         driver.switch_to.window(window)
-
-        # # Get Password Keypad Image
-        # pytesseract.pytesseract.tesseract_cmd = r'C:\Users\gks83\AppData\Local\tesseract.exe'
-        # kpdImg = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'kpd-image-button')))
-        # keypad = wait.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'kpd-data')))
-        # response = requests.get(kpdImg.get_attribute('src'))
-        # img = Image.open(io.BytesIO(response.content))
-
-        # # Keypad to Array
-        # txt = pytesseract.image_to_string(img, config='--psm 6')
-        # txtArr = re.findall(r'\d', txt)
-        # numbers = sorted(set(txtArr), key=txtArr.index)
-
-        # keypad = list(filter(lambda pad: pad.get_attribute('data-action').split(":")[0] == 'data', keypad))
-        # for factor in fa:
-        #     for idx, num in enumerate(numbers):
-        #         if factor == num:
-        #             keypad[idx].click()
-        
-        # time.sleep(1)
-        # Alert(driver).accept()
-        # driver.switch_to.window(parent)
+# =================== =================== ===================
+# Change Iframe
+# =================== =================== ===================
+driver.get(url='https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
+iframe = wait.until(EC.visibility_of_element_located((By.ID, 'ifrm_tab')))
+driver.switch_to.frame(iframe)
+print("[Log] Enter the Game")
 
 
 
 
 
-    # =================== =================== ===================
-    # Change Iframe
-    # =================== =================== ===================
-    driver.get(url='https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
-    iframe = wait.until(EC.visibility_of_element_located((By.ID, 'ifrm_tab')))
-    driver.switch_to.frame(iframe)
-    print("[Log] Enter the Game")
+# =================== =================== ===================
+# Auto Check & Buy Game
+# =================== =================== ===================
+ticket  = wait.until(EC.visibility_of_element_located((By.ID, 'divWay2Buy1')))
+setNum  = wait.until(EC.visibility_of_element_located((By.ID, 'btnSelectNum')))
+buyBtn  = wait.until(EC.visibility_of_element_located((By.ID, 'btnBuy')))
+btnWrap = ticket.find_element(By.CLASS_NAME, 'action')
+label = btnWrap.find_element(By.TAG_NAME, 'label')
+
+label.click()
+setNum.click()
+buyBtn.click()
+
+confirm = wait.until(EC.visibility_of_element_located((By.ID, 'popupLayerConfirm')))
+confirm.find_elements(By.TAG_NAME, 'input')[0].click()
+print("[Log] Buy Success")
 
 
 
 
 
-    # =================== =================== ===================
-    # Auto Check & Buy Game
-    # =================== =================== ===================
-    ticket  = wait.until(EC.visibility_of_element_located((By.ID, 'divWay2Buy1')))
-    setNum  = wait.until(EC.visibility_of_element_located((By.ID, 'btnSelectNum')))
-    buyBtn  = wait.until(EC.visibility_of_element_located((By.ID, 'btnBuy')))
-    btnWrap = ticket.find_element(By.CLASS_NAME, 'action')
-    label = btnWrap.find_element(By.TAG_NAME, 'label')
+# =================== =================== ===================
+# Record Lotto Number
+# =================== =================== ===================
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+report = wait.until(EC.visibility_of_element_located((By.ID, 'reportRow')))
+spans = report.find_element(By.CLASS_NAME, 'nums').find_elements(By.TAG_NAME, 'span')
 
-    label.click()
-    setNum.click()
-    buyBtn.click()
+number = '[Log][' + now + '] Number : '
+for span in spans:
+    number += span.get_attribute('innerHTML') + " "
 
-    confirm = wait.until(EC.visibility_of_element_located((By.ID, 'popupLayerConfirm')))
-    confirm.find_elements(By.TAG_NAME, 'input')[0].click()
-    print("[Log] Buy Success")
-
-
-
-
-
-    # =================== =================== ===================
-    # Record Lotto Number
-    # =================== =================== ===================
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    report = wait.until(EC.visibility_of_element_located((By.ID, 'reportRow')))
-    spans = report.find_element(By.CLASS_NAME, 'nums').find_elements(By.TAG_NAME, 'span')
-
-    number = '[Log][' + now + '] Number : '
-    for span in spans:
-        number += span.get_attribute('innerHTML') + " "
-
-    print(number)
-
-except Exception as e:
-    print(e)
+print(number)
